e74803ee6d956e027d6bbcc05eff830b081d6c5d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-02-01 03:12:38 -0500

    Arranque

--- bin/u.sh
+++ bin/u.sh
@@ -3,27 +3,37 @@
 #   necesarias para ejecutar como instalar gemas, generar recursos, 
 #   actualizar indices, etc.  Ver bin/corre
 
+if (test -f .env) then {
+  . ./.env
+} fi;
+
+if (test "${CONFIG_HOSTS}" = "") then {
+  echo "Definir variable de ambiente CONFIG_HOSTS"
+  exit 1;
+} fi;
 if (test "${SECRET_KEY_BASE}" = "") then {
-	echo "Definir variable de ambiente SECRET_KEY_BASE"
-	exit 1;
+  echo "Definir variable de ambiente SECRET_KEY_BASE"
+  exit 1;
 } fi;
 if (test "${USUARIO_AP}" = "") then {
-	echo "Definir usuario con el que se ejecuta en USUARIO_AP"
-	exit 1;
+  echo "Definir usuario con el que se ejecuta en USUARIO_AP"
+  exit 1;
 } fi;
 if (test "${DIRAP}" = "") then {
-	echo "Definir directorio de la aplicación en DIRAP"
-	exit 1;
+  echo "Definir directorio de la aplicación en DIRAP"
+  exit 1;
 } fi;
 
 defuroot=""
 if (test "${RAILS_RELATIVE_URL_ROOT}" != "") then {
-	defuroot="RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}"
+  defuroot="RAILS_RELATIVE_URL_ROOT=${RAILS_RELATIVE_URL_ROOT}"
 } fi;
 
 DOAS=`which doas 2> /dev/null`
 if (test "$?" != "0") then {
-	DOAS="sudo"
+  DOAS="sudo"
 } fi;
-$DOAS su - ${USUARIO_AP} -c "cd $DIRAP; echo \"== Iniciando unicorn... ==\"; ${defuroot} PUERTOUNICORN=${PUERTOUNICORN} CONFIG_HOSTS=${CONFIG_HOSTS} DIRAP=$DIRAP RAILS_ENV=production SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec /usr/local/bin/unicorn_rails -c $DIRAP/config/unicorn.conf.minimal.rb  -E production -D"
+$DOAS su - ${USUARIO_AP} -c "cd $DIRAP; 
+echo \"== Iniciando unicorn... ==\"; 
+${defuroot} PUERTOUNICORN=${PUERTOUNICORN} RUTA_RELATIVA=${RUTA_RELATIVA} CONFIG_HOSTS=${CONFIG_HOSTS} DIRAP=$DIRAP RAILS_ENV=production SECRET_KEY_BASE=${SECRET_KEY_BASE} bundle exec /usr/local/bin/unicorn_rails -c $DIRAP/config/unicorn.conf.minimal.rb  -E production -D"
 
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ Bundler.require(*Rails.groups)
 
 module Cor1440
   class Application < Rails::Application
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -25,15 +26,20 @@ module Cor1440
     config.active_record.schema_format = :sql
 
     config.x.url_colchon = ENV['COLCHON_ARTICULOS'] || 'colchon-articulos'
+    puts "OJO application.rb config.x.url_colchon=#{config.x.url_colchon}"
 
     config.x.formato_fecha = ENV['FORMATO_FECHA'] || 'dd/M/yyyy'
+    puts "OJO application.rb config.x.formato_fecha=#{config.x.formato_fecha}"
 
     config.relative_url_root = ENV['RUTA_RELATIVA'] || "/cor1440"
+    puts "OJO application.rb config.x.relative_url_root=#{config.x.relative_url_root}"
 
     config.x.heb412_ruta = ENV['HEB412_RUTA'] && ENV['HEB412_RUTA'] != '' ?
       Pathname(ENV['HEB412_RUTA']) : Rails.root.join('public', 'heb412')
+    puts "OJO application.rb config.x.heb412_ruta=#{config.x.heb412_ruta}"
 
-    config.hosts << ENV['CONFIG_HOSTS'] || '127.0.0.1'
+    config.hosts <<  (ENV['CONFIG_HOSTS'] ? ENV['CONFIG_HOSTS'].downcase : 'nodos.pasosdeJesus.org'.downcase)
+    puts "OJO application.rb config.x.config.hosts=#{config.x.config.hosts}"
 
    end
 end
