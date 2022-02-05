a9414d6bf242b279e79d61238cdb746b9ec25989
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2022-02-04 19:37:53 -0500

    Prueba

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,3 +14,31 @@ class ActiveSupport::TestCase
     load "#{Rails.root}/db/seeds.rb"
   end
 end
+
+
+PRUEBA_PERSONA= {
+  nombres: "Nombres",
+  apellidos: "Apellidos",
+
+  anionac: 1980,
+  mesnac: 2,
+  dianac: 2,
+  sexo: "M",
+  numerodocumento: "1061000000",
+}
+
+
+PRUEBA_USUARIO_OP = {
+  nusuario: "operador",
+  password: "sjrcol123",
+  nombre: "operador",
+  descripcion: "operador",
+  rol: 5,
+  idioma: "es_CO",
+  email: "operador@localhost",
+  encrypted_password: '$2a$10$uMAciEcJuUXDnpelfSH6He7BxW0yBeq6VMemlWc5xEl6NZRDYVA3G',
+  sign_in_count: 0,
+  fechacreacion: "2021-08-27",
+  fechadeshabilitacion: nil
+}
+
